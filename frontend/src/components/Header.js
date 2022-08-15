cc1244763485b0b2f5581e3106373a35b989bbec
leocairos <leocairos@gmail.com>
2022-08-15 20:47:13 -0300

    feat: Update logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Typography, Button } from '@material-ui/core/';
+import { Grid, Typography, Button, makeStyles } from '@material-ui/core/';
 import Cart from './Cart';
 
+const useStyles = makeStyles((theme) => ({
+    logo: {
+      width: '200px',
+      margin: '15px',
+    },
+  }));
+
 const Header = () => {
+    const classes = useStyles();
+
     return(
         <Grid container direction="row" justify="space-between" alignItems="center" xs={12}>
             <Typography variant='h3'>
-                Dio Shopping
+            <img className={classes.logo} src="./images/dio_shopping.svg" alt="DIO Shopping Logo"/>
             </Typography>
             <Link to="/">
                 <Button color="primary">Home</Button>
